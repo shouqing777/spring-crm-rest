0e096386667df51ea51648f269175e95b3d7e349
Shouqing Chen <shouqing777@gmail.com>
2018-09-22 19:26:26 +0800

    second

--- spring-crm-rest/src/main/java/com/luv2code/springdemo/config/DemoAppConfig.java
+++ spring-crm-rest/src/main/java/com/luv2code/springdemo/config/DemoAppConfig.java
@@ -16,11 +16,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
@@ -36,8 +33,6 @@ public class DemoAppConfig implements WebMvcConfigurer {
 	
 	private Logger logger = Logger.getLogger(getClass().getName());
 	
-	// define a bean for ViewResolver
-
 	@Bean
 	public DataSource myDataSource() {
 		
